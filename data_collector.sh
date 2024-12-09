b0fac7e8483a1c1456aa27db5d9125e501e9c031
Sangeetha Grama Srinivasan <sangeethagramas@gmail.com>
2024-12-08 21:25:51 -0800

    Added 0.01 res to data collector

--- data_collector.sh
+++ data_collector.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DamBreakModel.json                   --output-dir bin/ --no-gui
-python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DamBreakModelDragons.json            --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DamBreakModelWithCylinderTorus.json  --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DamBreakModelWithSphereOloid.json    --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DoubleDamBreak.json                  --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelArmadillo.json             --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelCylinder.json              --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelOloid.json                 --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelDragon.json                --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelTorus.json                 --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/DamBreakModel.json                   --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/DamBreakModelDragons.json            --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/DamBreakModelWithCylinderTorus.json  --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/DamBreakModelWithSphereOloid.json    --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/DoubleDamBreak.json                  --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/FluidModelArmadillo.json             --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/FluidModelCylinder.json              --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/FluidModelOloid.json                 --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/FluidModelDragon.json                --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.01/FluidModelTorus.json                 --output-dir bin/ --no-gui
 
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DamBreakModel.json                   --output-dir bin/ --no-gui
-python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DamBreakModelDragons.json            --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DamBreakModelWithCylinderTorus.json  --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DamBreakModelWithSphereOloid.json    --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DoubleDamBreak.json                  --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelArmadillo.json             --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelCylinder.json              --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelOloid.json                 --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelDragon.json                --output-dir bin/ --no-gui
-# python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeethag/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelTorus.json                 --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DamBreakModel.json                   --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DamBreakModelDragons.json            --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DamBreakModelWithCylinderTorus.json  --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DamBreakModelWithSphereOloid.json    --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/DoubleDamBreak.json                  --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelArmadillo.json             --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelCylinder.json              --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelOloid.json                 --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelDragon.json                --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.02/FluidModelTorus.json                 --output-dir bin/ --no-gui
+
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DamBreakModel.json                   --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DamBreakModelDragons.json            --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DamBreakModelWithCylinderTorus.json  --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DamBreakModelWithSphereOloid.json    --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/DoubleDamBreak.json                  --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelArmadillo.json             --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelCylinder.json              --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelOloid.json                 --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelDragon.json                --output-dir bin/ --no-gui
+python3 pySPlisHSPlasH/examples/custom_exporter.py /home/sangeetha/projects/SPlisHSPlasH/data/Scenes_data_gen_pr_0.04/FluidModelTorus.json                 --output-dir bin/ --no-gui
 
